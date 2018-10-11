09db0997f35ab4f05ad3b024959bf895e6063bb9
Akshay Blog WebApp <akshayboddhul45@gmail.com>
2018-10-11 16:45:13 +0530

    Added one product page with details

--- assets/js/main.js
+++ assets/js/main.js
@@ -56,28 +56,26 @@ jQuery(document).ready(function ($) {
 // slick slider active Home Page Tow
     $(".hello_slid").slick({
         dots: true,
-        infinite: false,
+        infinite: true,
         slidesToShow: 1,
         slidesToScroll: 1,
         arrows: true,
         prevArrow: "<i class='icon icon-chevron-left nextprevleft'></i>",
         nextArrow: "<i class='icon icon-chevron-right nextprevright'></i>",
-        autoplay: true,
-        autoplaySpeed: 2000
+        autoplay: false
     });
 
 
 
     $(".business_items").slick({
         dots: true,
-        infinite: false,
+        infinite: true,
         slidesToShow: 1,
         slidesToScroll: 1,
         arrows: true,
         prevArrow: "<i class='icon icon-chevron-left nextprevleft'></i>",
         nextArrow: "<i class='icon icon-chevron-right nextprevright'></i>",
-        autoplay: true,
-        autoplaySpeed: 2000
+        autoplay: false
     });
 
 
